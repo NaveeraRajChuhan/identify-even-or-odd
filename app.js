4db1b6dfeaecb6f0709ff737f01485a4082fec86
NaveeraRajChuhan <nnfstars@gmail.com>
2025-03-16 14:30:44 +0500

    if

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let userNumber1  = +prompt("enter first number");
 // let userNumber2  = +prompt("enter second number");
 // let findingEvenOdd = userNumber1 % 2==0
+if (userNumber1 % 2==0){
+    document.write("even")
+
+}
